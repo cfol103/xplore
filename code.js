a96d5e902cfb7611944644d35a2cb261471eccf0
Estefania Ortiz <eaortiz@stanford.com>
2013-07-12 22:59:36 -0700

    lat long

--- code.js
+++ code.js
@@ -6,4 +6,34 @@ $('.clickable').bind('click', function (ev) {
     var x = ev.clientX - offset.left;
     var y = ev.clientY - offset.top;
 
+     var lat = 180 * y / screen.height;
+	 var lon = 360 * x / screen.width;
+
+	 pixel = new VEPixel(x, y);
+            LL = map.PixelToLatLong(pixel);
+
+            map.FindLocations(LL, GetResults);
+
 });
+
+function GetMap() {
+   	var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), {credentials:"Your Bing Maps Key"});
+}
+
+
+ function GetResults(locations) {
+           var s="Results for " + LL.Latitude + ", " + LL.Longitude + ": ";
+            if(locations != null)
+            {
+              s+=locations[0].Name;
+              // Get the country name
+
+// open the hyperlink for the country
+            }
+            else
+            {
+               s+="No Result found.";
+            }
+
+            alert(s);
+}
