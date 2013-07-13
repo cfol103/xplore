4f697b2ef24ee25aed50764651153dc203c0323a
Carlos Folgar <folgar.tech@gmail.com>
2013-07-13 00:03:06 -0700

    Delete code.js
    deleting duplicate code.js

--- static/js/code.js
+++
@@ -1,9 +0,0 @@
-var clicks = [],
-$('.clickable').bind('click', function (ev) {
-    var $div = $(ev.target);
-    
-    var offset = $div.offset();
-    var x = ev.clientX - offset.left;
-    var y = ev.clientY - offset.top;
-
-});
